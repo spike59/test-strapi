9c27606a629418365d83fa89874ffc58fec58413
adam <adam.kassir@gmail.com>
2020-06-18 22:46:14 +0200

    mis a jour des fichiers de connection avec separation entre dev et prod

--- config/database.js
+++ config/database.js
@@ -4,12 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
+        client: env('DATABASE_CLIENT','sqlite'),
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
+
       },
       options: {
         ssl: false,
